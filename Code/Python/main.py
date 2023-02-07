a6aa3bf42a93b5c6705d429f00271efa07aafea5
Snythlord <96640521+Snythlord@users.noreply.github.com>
2023-02-07 18:31:32 -0500

    Made the alexa bot work with more commands

--- Code/Python/main.py
+++ Code/Python/main.py
@@ -1,6 +1,9 @@
 import speech_recognition as sr
 import pyttsx3 as tts
-
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
 
 listener = sr.Recognizer()
 engine = tts.init()
@@ -14,18 +17,62 @@ def talk(text):
 def take_command():
     try:
         with sr.Microphone() as source:
-            print('Listening...')
+            talk('Listening...')
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'alexa' in command:
+            if 'alexa' or 'hey' or 'yo' in command:
+                command = command.replace('yo', '')
+                command = command.replace('hey', '')
                 command = command.replace('alexa', '')
-                talk(command)
+
     except:
         pass
     return command
 
 def run_alexa():
     command = take_command()
-    if 'play' or 'song' or 'sing' in command:
-        talk('Playing your song!')
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('Playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk("The current time is " + time)
+    elif 'search' in command:
+        info = command.replace('search', '')
+        if "about" in command:
+            info = info.replace('about', "")
+        info = wikipedia.summary(info, 4)
+        talk(info)
+    elif 'who' in command:
+        info = command.replace('who', '')
+        if "is" in command:
+            info = info.replace('is', "")
+        info = wikipedia.summary(info, 4)
+        talk(info)
+    elif 'what' in command:
+        info = command.replace('who', '')
+        if "is" in command:
+            info = info.replace('is', "")
+        info = wikipedia.summary(info, 4)
+        talk(info)
+    elif 'how' in command:
+        info = command.replace('who', '')
+        if "is" in command:
+            info = info.replace('is', "")
+        info = wikipedia.summary(info, 4)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk("Please say that again.")
+
+
+    
+while True:
+    run_alexa()
+    command = take_command()
+    if "bye" in command:
+        talk("Goodbye!")
+        break
